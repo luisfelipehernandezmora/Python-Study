6a578a000540ed7f3444dabe6c0d9d46ff65d65b
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2022-02-04 22:06:19 -0600

    First lab done API

--- python_apis_databases-master/apis/Exercise_01.py
+++ python_apis_databases-master/apis/Exercise_01.py
@@ -8,7 +8,13 @@ Print out:
     - the status code
     - the encoding of the response
     - the text of the response body
-
-
-
 '''
+import requests
+from pprint import pprint
+
+url='http://demo.codingnomads.co:8080/tasks_api/users'
+request=requests.get(url)
+codigo=request.status_code
+texto=request.json()
+print("the status code is",codigo)
+pprint(texto)
