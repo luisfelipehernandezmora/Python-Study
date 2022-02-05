f61e48b906b3278e070f6d7241c1f27e8896f15b
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2022-02-04 22:41:19 -0600

    5th lab on deleting users with the Id

--- python_apis_databases-master/apis/Exercise_05.py
+++ python_apis_databases-master/apis/Exercise_05.py
@@ -4,3 +4,15 @@ Write a program that makes a DELETE request to remove the user your create in a 
 Again, make a GET request to confirm that information has been deleted.
 
 '''
+from pprint import pprint
+import requests
+
+url="http://demo.codingnomads.co:8080/tasks_api/users"
+id=input("Enter your user Id that you will be deleting: ")
+
+#Here You delete that register
+request=requests.delete(url+"/"+id)
+
+#Here to verify all is correct
+request=requests.get(url).json()
+pprint(request)
