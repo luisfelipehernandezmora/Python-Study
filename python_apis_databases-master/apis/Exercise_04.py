8e119ac46113cbc4e82bdb514a27e0ab1a0e632b
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2022-02-04 22:34:19 -0600

    4th lab on PUT and update values from the users link

--- python_apis_databases-master/apis/Exercise_04.py
+++ python_apis_databases-master/apis/Exercise_04.py
@@ -4,3 +4,27 @@ Write a program that makes a PUT request to update your user information to a ne
 Again make a GET request to confirm that your information has been updated.
 
 '''
+import requests
+from pprint import pprint
+
+#These will be updating information for an already existing user
+name=input("Enter your name: ")
+last_name=input("Enter your last name: ")
+email=input("Enter your email adress: ")
+
+#We make a new body, to insert the information as a new register
+body={
+    "first_name": name,
+    "last_name": last_name,
+    "email": email
+}
+#With the Id we will know which user to modify
+id=input("Enter your user Id that you will be modyfing: ")
+
+#Make the PUT request, important the "/" symbol in the ID
+url="http://demo.codingnomads.co:8080/tasks_api/users"
+request=requests.put(url+"/"+id, json=body)
+
+#Check all went fine by printing here itself
+request=requests.get(url).json()
+pprint(request)
