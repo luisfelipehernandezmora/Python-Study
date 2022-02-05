f4ea6dbf377c04cc854f5fa98b804a0291642f7d
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2022-02-04 22:10:10 -0600

    Second lab done API

--- python_apis_databases-master/apis/Exercise_02.py
+++ python_apis_databases-master/apis/Exercise_02.py
@@ -3,3 +3,15 @@ Building on the previous example, create a list of all of the emails of the user
 the list to the console.
 
 '''
+from distutils import core
+import requests
+from pprint import pprint
+
+url='http://demo.codingnomads.co:8080/tasks_api/users'
+request=requests.get(url)
+codigo=request.status_code
+texto=request.json()["data"]
+correos=[]
+for x in texto:
+    correos.append(x["email"])
+pprint(correos)
