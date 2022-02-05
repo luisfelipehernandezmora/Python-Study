2aa4047bd82d59c6c5ec430f995d55db2df2eada
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2022-02-04 22:20:18 -0600

    Third lab, posting and checking with a get request

--- python_apis_databases-master/apis/Exercise_03.py
+++ python_apis_databases-master/apis/Exercise_03.py
@@ -8,3 +8,21 @@ and create a user with your information.
 Make a GET request to confirm that your user information has been saved.
 
 '''
+import requests
+from pprint import pprint
+
+url="http://demo.codingnomads.co:8080/tasks_api/users"
+
+name=input("Enter your name: ")
+last_name=input("Enter your last name: ")
+email=input("Enter your email adress: ")
+
+body={
+    "first_name": name,
+    "last_name": last_name,
+    "email": email
+}
+request=requests.post(url, json=body)
+
+request=requests.get(url).json()
+pprint(request)
