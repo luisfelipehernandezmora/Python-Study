f4a2fd1bf03912a17cac66f091a26eff5e45c7c5
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2022-02-05 20:33:33 -0600

    Exercise 1 finished and functional

--- python_apis_databases-master/apis/extra_tasks/01_countries.py
+++ python_apis_databases-master/apis/extra_tasks/01_countries.py
@@ -15,11 +15,23 @@ url="https://restcountries.com/v3.1"
 country1=input("Which country you want to know about? ")
 country2=input("Which other country you want to know about? ")
 
-name1=requests.get(url+"/name/"+country1).json()
-pprint(name1)
+name1=requests.get(url+"/name/"+country1).json()[0]["name"]["common"]
 
+if name1 == country1:
+    population1=requests.get(url+"/name/"+country1).json()[0]["population"]
+    area1=requests.get(url+"/name/"+country1).json()[0]["area"]
 
+name2=requests.get(url+"/name/"+country2).json()[0]["name"]["common"]
+if name1 == country1:
+    population2=requests.get(url+"/name/"+country2).json()[0]["population"]
+    area2=requests.get(url+"/name/"+country2).json()[0]["area"]
 
-request=requests.get(url).json()
+if population1>population2:
+    print(f"{country1} have a bigger population than {country2}, {population1} against {population2}")
+else:
+    print(f"{country2} have a bigger population than {country1}, {population2} against {population1}")
 
-
+if area1>area2:
+    print(f"{country1} have a bigger territory than {country2}, {area1} against {area2}")
+else:
+    print(f"{country2} have a bigger territory than {country1}, {area2} against {area1}")
