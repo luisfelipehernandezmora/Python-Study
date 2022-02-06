40c0955352708b4fd7e26984282cc78d0b5590ae
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2022-02-05 19:18:21 -0600

    begining the exercise 1

--- python_apis_databases-master/apis/extra_tasks/01_countries.py
+++ python_apis_databases-master/apis/extra_tasks/01_countries.py
@@ -6,5 +6,20 @@ In your python program, parse and compare the data of the two responses:
 * Which country has the larger population?
 * How much does the are of the two countries differ?
 * Print the native name of both countries, as well as their capitals
-
 '''
+import requests
+from pprint import pprint
+
+url="https://restcountries.com/v3.1"
+
+country1=input("Which country you want to know about? ")
+country2=input("Which other country you want to know about? ")
+
+name1=requests.get(url+"/name/"+country1).json()
+pprint(name1)
+
+
+
+request=requests.get(url).json()
+
+
