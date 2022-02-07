9f64c3304d9316898d99286374adc5659606e488
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2022-02-07 08:36:03 -0600

    Began the bitcoin exercise, have to get the right end point

--- python_apis_databases-master/apis/extra_tasks/05_crypto.py
+++ python_apis_databases-master/apis/extra_tasks/05_crypto.py
@@ -14,3 +14,35 @@ HINTS:
 BONUS: Explore the logging package for easier tracking
 
 '''
+import requests
+from pprint import pprint
+# url="https://api.nomics.com/v1/currencies/ticker?key=3500ecce876792a90a078c86001b3cc7d5a78986&ids=BTC,ETH,XRP&interval=1d,30d&convert=EUR&platform-currency=ETH&per-page=100&page=1"
+# request=requests.get(url).json()["price"]
+# pprint(request)
+
+
+# import urllib.request
+# url = """https://api.nomics.com/v1/currencies/ticker?
+# key=your-key-here
+# &ids=BTC,ETH,XRP
+# &interval=1d,30d
+# &convert=EUR
+# &platform-currency=ETH
+# &per-page=100
+# &page=1"""
+# print(urllib.request.urlopen(url).read())
+
+params={
+    "key":"3500ecce876792a90a078c86001b3cc7d5a78986",
+    "ids": "BTC,ETH", #"BTC,ETH,XRP
+    "interval":"1d,30d",
+    "convert":"EUR",
+    "platform-currency":"ETH",
+    "per-page":100,
+    "page":1,
+}
+url="https://api.nomics.com/v1/currencies/ticker"
+response=requests.get(url, params=params)
+pprint(response.json()[0]["price"])
+#a=response.json()
+print(response.status_code)
