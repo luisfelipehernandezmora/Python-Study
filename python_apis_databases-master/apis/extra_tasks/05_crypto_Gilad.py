80752007da0a21a2a88127564c564597f756c59b
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2022-02-07 21:16:21 -0600

    cleaner files

---
+++ python_apis_databases-master/apis/extra_tasks/05_crypto_Gilad.py
@@ -0,0 +1,34 @@
+'''
+http://docs.nomics.com/
+Using the nomics API, repeatedly fetch the price of Bitcoin for a duration of 10 minutes.
+Store each value in a dictionary that uses the time of query as a key.
+
+After the script stopped running, determine programmatically at what query time the price
+was the highest, and when the lowest.
+
+HINTS:
+- request an API key first and remember to include it in your queries
+- the /prices endpoint of the nomics API can be used for achieving this task
+- remember to use packages from the standard library, e.g. for time keeping and dates
+
+BONUS: Explore the logging package for easier tracking
+
+'''
+
+import requests
+from pprint import pprint
+
+#Session on how to search using request library and packages, but was on the worng endpoint /prices
+params={
+    "key":"3500ecce876792a90a078c86001b3cc7d5a78986",
+    "ids": "BTC,ETH", #"BTC,ETH,XRP
+    "interval":"1d,30d",
+    "convert":"EUR",
+    "platform-currency":"ETH",
+    "per-page":100,
+    "page":1,
+}
+url="https://api.nomics.com/v1/currencies/ticker"
+response=requests.get(url, params=params)
+pprint(response.json()[0]["price"])
+print(response.status_code)
