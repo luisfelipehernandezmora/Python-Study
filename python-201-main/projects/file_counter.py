65b367acca46bc8ae6e67d2e37c3787eae88dc9a
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2022-02-11 21:49:20 -0600

    file counter exercise done

--- python-201-main/projects/file_counter.py
+++ python-201-main/projects/file_counter.py
@@ -1,18 +1,24 @@
 # Add the code for the file counter script that you wrote in the course.
+from itertools import count
 import pathlib
+import csv
 #  \\wsl$\Ubuntu-20.04\home\luisfelipe\Coding Nomads\python-201-main\projects
 folder=pathlib.Path("/home/luisfelipe/Coding Nomads/python-201-main/projects")
-#for file in folder.iterdir():
-
-a=["dot.txt", "image.png", "python.20.32.19.py"]
-extensiones=set()
+extensiones=[]
 cantidades=dict()
-#for x in a: 
 for file in folder.iterdir():
         file_name=file.name
         c=file_name.split(".") 
         ext="."+c[-1]
         #print(c,ext) 
-        extensiones.add(ext)
-        
+        extensiones.append(ext)
 print(extensiones)
+for elem in extensiones:
+        cantidades[elem]=extensiones.count(elem)
+print(cantidades)
+
+for key in cantidades.keys():
+        data = [key,cantidades[key]]
+        with open ("file_counter.csv","a") as file:
+                writer = csv.writer(file)
+                writer.writerow(data)
