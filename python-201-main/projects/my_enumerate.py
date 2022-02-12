675e5b4285bc524c8abfc7963c38eaeb9fc81058
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2022-02-11 22:15:35 -0600

    My_enumerate function done

--- python-201-main/projects/my_enumerate.py
+++ python-201-main/projects/my_enumerate.py
@@ -2,6 +2,20 @@
 # Define a function called `my_enumerate()` that takes an iterable as input
 # and gives back both the element as well as its index position as an integer.
 
-def my_enumerate():  # add your arguments
-      # add your code implementation
-      pass
+def my_enumerate(list): 
+      n=len(list)
+      new=[]
+      for i in range(n):
+            temp=()
+            temp=i,list[i]
+            new.append(temp)          
+      return(new)
+
+lista=["uno","dos","tres","cuatro","cinco","seis","siete","ocho"]
+
+c=my_enumerate(lista)
+print(c,"own function")
+
+d=enumerate(lista,start=0)
+for i in d:
+      print(i)
