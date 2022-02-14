f1798b051e1bac0e3ce01e893a9b6bc473e4f177
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2022-02-14 15:57:02 -0600

    Exercise 2 done

--- python_apis_databases-master/databases/Exercise_02.py
+++ python_apis_databases-master/databases/Exercise_02.py
@@ -30,8 +30,8 @@ question=int(input("""What you will like to do?
 2) Select all the actors and the films they have been in
 3) Select all the actors that have appeared in a category of your choice
 4) Select all the comedic films and sort them by rental rate
-5) Using one of the statements above, add a GROUP BY statement of your choice
-6) Using one of the statements above, add a ORDER BY statement of your choice
+5) See how many actors participated in each comedy movie (using group)
+6) Select all the comedic films and sort them by rental rate (using order by)
 """))
 
 ' 1) Select all the actors with the first name of your choice'
@@ -84,11 +84,14 @@ if question==4:
 
 '5) Using one of the statements above, add a GROUP BY statement of your choice' #***pending
 if question==5:
-    film_category=sqlalchemy.Table('film_category', metadata, autoload=True, autoload_with=engine)
+    film_actor=sqlalchemy.Table('film_actor', metadata, autoload=True, autoload_with=engine)
+    actor=sqlalchemy.Table('actor', metadata, autoload=True, autoload_with=engine)
     film=sqlalchemy.Table('film', metadata, autoload=True, autoload_with=engine)
-    join_statement=film.join(film_category, film_category.columns.film_id==film.columns.film_id)
-    query5=sqlalchemy.select([film.columns.title, film.columns.rental_rate]).where(film_category.columns.category_id==5).order_by(sqlalchemy.asc(film.columns.rental_rate)).select_from(join_statement)
-    response_proxy=conection.execute(query5)
+    film_category=sqlalchemy.Table('film_category', metadata, autoload=True, autoload_with=engine)
+    join_statement=actor.join(film_actor, film_actor.columns.actor_id==actor.columns.actor_id).join(film, film_actor.columns.film_id==film.columns.film_id).join(film_category, film_category.columns.film_id== film.columns.film_id)
+    query3=sqlalchemy.select([film.columns.title, sqlalchemy.func.count(actor.columns.actor_id)]).where(film_category.columns.category_id==5).select_from(join_statement).group_by(film.columns.title)
+
+    response_proxy=conection.execute(query3)
     response_set=response_proxy.fetchall()
     pprint(response_set)
 
