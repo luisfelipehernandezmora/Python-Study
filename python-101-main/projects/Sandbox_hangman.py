cc4f00556344183f503f537a28c8cd854d7d1934
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2022-02-19 21:52:17 -0600

    API request to translate the meaning to Spanish

--- python-101-main/projects/Sandbox_hangman.py
+++ python-101-main/projects/Sandbox_hangman.py
@@ -1,4 +1,10 @@
-lista=["foo", "bar", "baz","bar"]
-a=lista.index("bar")
-b=lista.index("bar",a+1)
-print(a,b)
+from pprint import pprint
+import requests
+word=input("Insert a word ")
+url="https://dictionaryapi.com/api/v3/references/spanish/json/"+word+"?key=fe594801-4f52-4e7d-9340-4317a163897a"
+
+response=requests.get(url).json()[0]
+definition=response["shortdef"]
+stem=response["meta"]["stems"]
+pprint(f"El significado de la palabra {word} es: {definition}")
+pprint(f"""{word} también tiene palabras derivadas como: {stem}, Estas se pueden usar en un monton de contextos, anotalas y practique oraciones en las que las puede utilizar!""")
