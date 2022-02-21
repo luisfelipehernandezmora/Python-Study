c53143ed6faa851414e0ae98eb0170a51d704a2c
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2022-02-20 22:10:22 -0600

    File mover

--- python-101-main/projects/mover.py
+++ python-101-main/projects/mover.py
@@ -1,14 +1,15 @@
 # Write a script that moves all files with the .png extension
 # from one folder to another
-
 # Import pathlib
-
 # Find the path to my Desktop
-
 # Create a new folder
-
 # Filter for screenshots only
-
 # Create a new path for each file
-
 # Move the screenshot there
+import pathlib
+folder=pathlib.Path("/home/luisfelipe/Coding Nomads/python-101-main/projects/move_files_from_here")
+destination=pathlib.Path("/home/luisfelipe/Coding Nomads/python-101-main/projects/move_here")
+for files in folder.iterdir():
+    ext=files.suffix
+    if ext==".png":
+        print(files)
