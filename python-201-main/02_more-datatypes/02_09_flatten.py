93d0bbc4ba0f6abd041781197681fc4df4c41f81
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2022-02-23 16:29:18 -0600

    Exercise 9 done!

--- python-201-main/02_more-datatypes/02_09_flatten.py
+++ python-201-main/02_more-datatypes/02_09_flatten.py
@@ -9,7 +9,7 @@
 # CHALLENGE: Do some research online and find a solution that works
 # to flatten a list of any depth. Can you understand the code used?
 
-starter_list = [[1, 2, 3, 4], [5, 6], [7, 8, 9]]
+#starter_list = [[1, 2, 3, 4], [5, 6], [7, 8, 9]]
 deep_list=[1,[2,4,"word"],"another word",[1,2,3,[10,11,12,[100,200]]],3,"happy ending",[-1,-2,[-10,-20,[1000]]]]
 
 # First simple way for shallow lists
@@ -21,13 +21,40 @@ def flattener(lista):
             flattened_list.extend(element)
         else:
             flattened_list.append(element)
-    print(f"The flattened list is {flattened_list}")
+    #print(f"The flattened list is {flattened_list}")
     return(flattened_list)
 
-flat=flattener(starter_list)
-print(flat)
+# flat=flattener(starter_list)
+# print(flat)
 
-# for element in starter_list:
-#     eslista=True
-#     if isinstance(element,list):
-#         eslista=False
+# Second complex way for deep lists
+flattened_list=[]
+for element in deep_list:
+    eslista=False
+    if isinstance(element,list):
+        eslista=True
+
+    if eslista==False:
+        flattened_list.append(element)
+        continue
+    else:
+        temp=flattener(element)
+        flated=False
+        temp=flattener(temp)
+        while flated==False:
+            temp2=[]
+            for elem2 in temp:
+                if type(elem2)==list:
+                    temp=flattener(temp)
+                    break
+                elif type(elem2)!=list:
+                    temp2.append(elem2)
+            flated=True
+        flattened_list.extend(temp2)
+print(f"The flattened list is {flattened_list}")
+
+                    
+
+            
+
+
