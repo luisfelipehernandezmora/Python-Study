23732982616d67e6e7b4645ae3e7c425132a8f40
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2022-02-23 15:56:36 -0600

    Exercise 8 done and 9 on the way

--- python-201-main/02_more-datatypes/02_08_remove_duplicates.py
+++ python-201-main/02_more-datatypes/02_08_remove_duplicates.py
@@ -3,4 +3,15 @@
 # 1. Convert to a different data type
 # 2. Use a loop and a second list to solve it more manually
 
-list_ = [1, 2, 3, 4, 3, 4, 5]
+list1= [1, 2, 3, 4, 3, 4, 5]
+# Way 1: To convert into a set type
+only=set(list1)
+print(f"first way, with a set {only}")
+
+# Way 2: Loop and a second list
+lista2=[]
+for elem in list1:
+    if elem in lista2:
+        continue
+    lista2.append(elem)
+print(f"second way, with a loop and a second list {lista2}")
--- python-201-main/02_more-datatypes/02_09_flatten.py
+++ python-201-main/02_more-datatypes/02_09_flatten.py
@@ -10,3 +10,24 @@
 # to flatten a list of any depth. Can you understand the code used?
 
 starter_list = [[1, 2, 3, 4], [5, 6], [7, 8, 9]]
+deep_list=[1,[2,4,"word"],"another word",[1,2,3,[10,11,12,[100,200]]],3,"happy ending",[-1,-2,[-10,-20,[1000]]]]
+
+# First simple way for shallow lists
+def flattener(lista):
+    """"Flatens shallow lists"""
+    flattened_list=[]
+    for element in lista:
+        if isinstance(element,list):
+            flattened_list.extend(element)
+        else:
+            flattened_list.append(element)
+    print(f"The flattened list is {flattened_list}")
+    return(flattened_list)
+
+flat=flattener(starter_list)
+print(flat)
+
+# for element in starter_list:
+#     eslista=True
+#     if isinstance(element,list):
+#         eslista=False
