38d1c90e5778abb9635eab9047507e01d32a8622
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2022-02-23 13:13:56 -0600

    Exercise 5 done and working

--- python-201-main/02_more-datatypes/02_05_split_and_pick.py
+++ python-201-main/02_more-datatypes/02_05_split_and_pick.py
@@ -1,17 +1,21 @@
 # Write a script that takes in a string from the user.
 # Using the `split()` method, create a list of all the words in the string
 # and print back the most common word in the text.
-from multiprocessing.sharedctypes import Value
-
 
 text=input("Insert a text ")
 lista=text.split(sep=" ")
 #print(lista)
 comon={}
+store=[]
 for word in lista:
     number_of_times=lista.count(word)
     comon[word]=number_of_times
 maxi=max(comon.values())
-print(maxi)  
+#print(maxi)  
 most_common=comon.items()
 print(most_common)
+for key in comon.keys():
+    if comon[key]==maxi:
+        store.append(key)
+print(f"The words that more appear in the text are {store} and appear {maxi} times")
+    
