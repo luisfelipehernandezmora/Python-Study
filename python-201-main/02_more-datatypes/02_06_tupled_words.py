0fd4dfdb9eeb66e2196a398d2333bd1518d915c6
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2022-02-23 14:48:06 -0600

    Exercise 6 and 7 done

--- python-201-main/02_more-datatypes/02_06_tupled_words.py
+++ python-201-main/02_more-datatypes/02_06_tupled_words.py
@@ -4,3 +4,13 @@
 
 # input = "hello world"
 # result_list = [('h', 'e', 'l', 'l', 'o'), ('w', 'o', 'r', 'l', 'd')]
+word=input("Insert your phrase to separate in a list of tuples: ")
+lista=[]
+result_list=[]
+lista=word.split(" ")
+for item in lista:
+    x=tuple(item)
+    result_list.append(x)
+print(f"The result list is {result_list}")
+
+
--- python-201-main/02_more-datatypes/02_07_convert_sequences.py
+++ python-201-main/02_more-datatypes/02_07_convert_sequences.py
@@ -5,3 +5,12 @@
 # - Convert the list back into a tuple.
 
 string = "codingnomads"
+strtup=tuple(string)
+lista=[]
+for ele in strtup:
+    lista.append(ele)
+for i in range(len(lista)):
+    if lista[i]=="c":
+        lista[i]="k"
+final_tuple=tuple(lista)
+print(f"The final tuple is {final_tuple}")
