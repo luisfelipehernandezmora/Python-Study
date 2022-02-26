96859c8118a2c1167f2f97d3eba5578f6c2ad26d
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2022-02-25 21:52:43 -0600

    Ex 4 solved

--- python-201-main/03_file-input-output/03_04_pathlib_reader.py
+++ python-201-main/03_file-input-output/03_04_pathlib_reader.py
@@ -3,9 +3,8 @@
 # path in a way so that you can run the script from anywhere.
 import os
 import pathlib
-from pathlib import Path
 import csv
-folder=pathlib.Path(r"C:/Users/Amma/Desktop/Coding Nomads/python-201-main/02_more-datatypes/Projects/files for the example_file counter")
+folder=pathlib.Path("/home/luisfelipe/Coding Nomads/python-201-main/03_file-input-output/files_to_count")
 types=set()
 conteo=[]
 cuenta=[]
@@ -13,13 +12,12 @@ dict={}
 for file in folder.iterdir():
     cuenta.append(file.suffix)
     types.add(file.suffix)
-for elem in types:
-    conteo.append(elem)
+conteo=list(types)
 for elem in conteo:
     dict[elem]=cuenta.count(elem)
 print(dict)
-path_write=pathlib.Path(r"C:\Users\Amma\Desktop\Coding Nomads\python-201-main\02_more-datatypes\Projects\files for the example_file counter\write_more_typesV2.csv")
-existente=os.path.isfile(r"C:\Users\Amma\Desktop\Coding Nomads\python-201-main\02_more-datatypes\Projects\files for the example_file counter\write_more_typesV2.csv")
+path_write=pathlib.Path("/home/luisfelipe/Coding Nomads/python-201-main/03_file-input-output/summary_types.csv")
+existente=os.path.isfile("/home/luisfelipe/Coding Nomads/python-201-main/03_file-input-output/summary_types.csv")
 if existente==False:
     with open(path_write,"w") as csvfile:
         header = dict.keys()
