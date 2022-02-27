8af7f83b771ff354428f5ba093088681b0303a65
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2022-02-27 11:53:39 -0600

    Ex 7 done

--- python-201-main/04_functions-and-scopes/04_07_determine_divisible.py
+++ python-201-main/04_functions-and-scopes/04_07_determine_divisible.py
@@ -7,6 +7,9 @@
 # - call your functions, passing in the user input as the arguments,
 #   and set their output equal to new variables 
 # - print your the result variables with descriptive messages
+import random
+
+
 def is_div(num):
     desicion=False
     if num%4==0:
@@ -16,3 +19,7 @@ def is_div(num):
     else:
         desicion=False
     return(desicion)
+#num=int(input("Introduce a number to see if it is divisible by 4 or 7 "))
+num=random.randint(0,10000000)
+result=is_div(num)
+print(f"So the number is {num} and is {result} divisible by 4 or 7")
