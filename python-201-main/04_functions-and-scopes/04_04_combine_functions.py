759076aebe304630d05016deb695b7a2663db0a6
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2022-02-26 21:36:13 -0600

    Ex 4 done

--- python-201-main/04_functions-and-scopes/04_04_combine_functions.py
+++ python-201-main/04_functions-and-scopes/04_04_combine_functions.py
@@ -2,3 +2,21 @@
 # with the `write_letter()` function from the previous exercise.
 # Write both functions in this script and call `greet()` within `write_letter()`
 # to let `greet()` take care of creating the greeting string.
+
+def greet(name):
+    hello=f"Hey {name}! Nice to see you around!"
+    return(hello)
+def write_letter(name, text):
+    
+    hola=greet(name)
+    body=f"""    
+    As always we love to comunicate important information in the right manner, so:
+    
+    {text}
+    
+    Thank you {name} for always being the excellent person you are    
+    """
+    return(hola+body)
+
+a=write_letter("All mother's children out there", "India open 3 months visas again!")
+print(a)
