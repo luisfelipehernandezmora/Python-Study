093c7c67ddcd00fdbfe9cf14883458ffd074fc8c
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2022-02-28 22:25:23 -0600

    done

--- python-201-main/06_advanced-python-concepts/06_15_baby_names.py
+++ python-201-main/06_advanced-python-concepts/06_15_baby_names.py
@@ -5,4 +5,21 @@
 # To save the day, use the filter() method and a lambda expression
 # to map all the baby names that begin with a 'M' to an output list.
 
+from unicodedata import name
+
+
 names = ['Olivia', 'Noah', 'Ava', 'Oliver', 'Isabella', 'Mason', 'Sophia', 'Logan', 'Emma', 'Liam', 'Amelia', 'Lucas', 'Mia', 'Elijah', 'Charlotte', 'Ethan', 'Harper', 'James', 'Mila', 'Aiden', 'Aria', 'Carter', 'Ella', 'Jackson', 'Evelyn', 'Alexander', 'Avery', 'Sebastian', 'Abigail', 'Michael', 'Emily', 'Benjamin', 'Luna', 'Jacob', 'Riley', 'William', 'Scarlett', 'Grayson', 'Chloe', 'Jack', 'Sofia', 'Daniel', 'Layla', 'Owen', 'Lily', 'Luke', 'Madison', 'Henry', 'Ellie', 'Wyatt', 'Zoey', 'Jayden', 'Elizabeth', 'Leo', 'Penelope', 'Gabriel', 'Victoria', 'Julian', 'Grace', 'Matthew', 'Nora', 'David', 'Aubrey', 'Jaxon', 'Camila', 'Levi', 'Hannah', 'Mateo', 'Bella', 'Asher', 'Aurora', 'Lincoln', 'Addison', 'John', 'Stella', 'Samuel', 'Skylar', 'Muhammad', 'Paisley', 'Ryan', 'Savannah', 'Adam', 'Maya', 'Isaac', 'Natalie', 'Nathan', 'Elena', 'Josiah', 'Emilia', 'Isaiah', 'Violet', 'Joseph', 'Hazel', 'Caleb', 'Nova', 'Anthony', 'Niamey', 'Hunter', 'Eva', 'Eli']
+
+def check_m(name):
+    if name[0]=="M":
+        return True
+    return False
+lista_nombres=[]
+m_names=filter(check_m,names)
+
+
+
+
+for x in m_names:
+    lista_nombres.append(x)
+print(lista_nombres)
