441747d0f161499716c06c28fe53fc8c9c37648a
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2022-02-28 22:31:15 -0600

    done

--- python-201-main/08_debugging/08_02_find_the_bugs.py
+++ python-201-main/08_debugging/08_02_find_the_bugs.py
@@ -3,12 +3,12 @@
 unsorted_list = [('first_element', 4), ('second_element', 2), ('third_element', 6)]
 sorted_list = []
 
-for x in range(1, len(unsorted_list)):
+for x in range(0, len(unsorted_list)):
 
-    minimum = unsorted_list[0][0]
+    minimum = unsorted_list[0][1]
     index = 0
 
-    for i in range(1, len(unsorted_list)):
+    for i in range(0, len(unsorted_list)):
         if unsorted_list[i][1] < minimum:
             minimum = unsorted_list[i][i]
             index = i
