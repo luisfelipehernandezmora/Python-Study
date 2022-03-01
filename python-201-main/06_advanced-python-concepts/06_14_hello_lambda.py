25db0b509f532d12efe47592c30777ad53ce8ddc
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2022-02-28 22:15:25 -0600

    done

--- python-201-main/06_advanced-python-concepts/06_14_hello_lambda.py
+++ python-201-main/06_advanced-python-concepts/06_14_hello_lambda.py
@@ -1,2 +1,6 @@
 # Create a lambda expression that takes no input
-# and prints "hello world" to the console. What does it return?
+# and prints "hello world" to the console. What does it return? It return NONE
+
+x=lambda :print("Hello!")
+x() 
+print(x())
