97f4c046189c81228def4bab59959ab4a5b8dd0e
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2022-03-05 22:02:08 -0600

    Solving issue on Car class to update speed

--- python-301-main/02_classes-objects-methods/02_03_car.py
+++ python-301-main/02_classes-objects-methods/02_03_car.py
@@ -6,3 +6,25 @@
 #
 # Create at least two different objects of this `Car()` class and demonstrate
 # changing the objects' attributes.
+class Car():
+    """Class of cars with basic mechanical information.
+
+    Args:
+        1st arg: model
+        2nd arg: year 
+        3rd arg: Max Speed in km/h
+          
+    Returns:
+        A car object with it's main features.
+        """
+    def __init__(self,model,year,max_speed):
+        self.model=model
+        self.year=year
+        self.max_speed=max_speed
+    def increase_speed(self):
+        self.max_speed+=5
+
+beetle=Car("volkswagen beetle",1970,100)
+
+modern=beetle.increase_speed()
+print(f"{modern.max_speed}")
