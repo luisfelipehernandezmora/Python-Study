a7771caf0f9c7b9b7a9b40bb96ff17592e7e072c
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2022-03-06 13:58:04 -0600

    Classy shapes done

--- python-301-main/02_classes-objects-methods/02_04_classy_shapes.py
+++ python-301-main/02_classes-objects-methods/02_04_classy_shapes.py
@@ -5,6 +5,10 @@
 # Write methods in the appropriate class so that you can calculate
 # the area of both the rectangle and the circle, the perimeter
 # of the rectangle, and the circumference of the circle.
+import math
+from turtle import circle
+
+
 class Rectangle():
     """Class to define a rectangle.
 
@@ -21,11 +25,32 @@ class Rectangle():
     def __str__(self) -> str:
         return(f"A rectangle to be defined by length and width")
     def basic_geometry (self):
-        area=self.length*self.width
-        perimeter=2*(self.length+self.width)
+        area=round(self.length*self.width,2)
+        perimeter=round(2*(self.length+self.width),2)
         return(f"The area of the rectangle is {area} \n and the perimeter is {perimeter}")
 
+class Circle():
+    """Class to define a circle.
+
+    Args:
+        1st arg: radius in the desired metric unit
+                
+    Returns:
+        The area in square (metric unit) and the circunference in metric unit.
+        """
+    def __init__(self,radius):
+        self.radius=radius
+    def __str__(self) -> str:
+        return(f"A circle to be defined by it's radius")
+    def basic_geometry(self):
+        area=round(math.pi*(self.radius)**2,2) 
+        perimeter=round(2*math.pi*self.radius,2)
+        return(f"The area of the circle is {area} \n and the circunference is {perimeter}")
+
 rectangulo=Rectangle(15,10).basic_geometry()
-print(rectangulo)
+circulo=Circle(4).basic_geometry()
+print("\n",rectangulo,"\n")
+print(circulo)
+
 
         
