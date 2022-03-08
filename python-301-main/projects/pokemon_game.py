14a66ef3c01a37ae18c55e920c899e95c4091237
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2022-03-07 22:31:58 -0600

    Pokemon class began

--- python-301-main/projects/pokemon_game.py
+++ python-301-main/projects/pokemon_game.py
@@ -11,3 +11,27 @@
 # - Display messages that explain who won or lost a battle
 # - If a Pokemon loses a battle, they lose some of their `hp`
 # - If you call the `feed()` method on a Pokemon, they regain some `hp`
+class Pokemon:
+    """Class to create Pokemons
+
+    Args:
+        1st arg: name
+        2nd arg: Type (water, fire or grass!) 
+                     
+    Returns:
+    A list of information of the country in order: [name, veg/non veg, calories, price]
+    """ 
+    def __init__(self, name, type, hp, max_hp):
+        self.name=name
+        self.type=type
+        self.hp=hp
+        self.max_hp=max_hp
+    
+    def __str__(self) -> str:
+        return(f"Pokemon(name = {self.name}, Type = {self.type}, hp = {self.hp}, max_hp = {self.max_hp})")
+    
+    def feed(self):
+        if self.hp<self.max_hp/2 :
+            self.hp+=10
+        else:
+            self.hp+=5
