8a9bbcac3a85316041b0d758c6857b81a5ef10a9
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2022-03-09 22:47:44 -0600

    Inheritance added

--- python-301-main/02_classes-objects-methods/Ingredients.py
+++ python-301-main/02_classes-objects-methods/Ingredients.py
@@ -8,10 +8,10 @@ class Ingredient:
     def expire(self):
         """Expires the ingredient"""
         print(f"Oh no! these {self.name} got expired!")
-        self.name="expired" +self.name +":/" #Here you change the name from tomatoes to expired tomatoes
+        self.name="expired " +self.name +" :/" #Here you change the name from tomatoes to expired tomatoes
     
     def __str__(self):
-        return(f"{self.name}({self.amount})")
+        return(f"{self.name} ({self.amount})")
         #return(f"There are {self.amount} {self.name}")
     
     def __repr__(self):
@@ -28,14 +28,19 @@ class Ingredient:
         url="https://en.wikipedia.org/wiki/"+self.name
         webbrowser.open(url, new=0, autoraise=True)
 
+class Spice(Ingredient):
+
+    pass
+
+lentils=Spice("Lentils",5)
+lentils.expire()
+print(lentils)
+
 if __name__=="main":
     papas=Ingredient("potatoes",5)
     cookies=Ingredient("cookies",2)
     water=Ingredient("Special water",3)
-
     zanahorias=papas+cookies
-
     print("",papas.name,papas.amount,"\n",cookies.name,cookies.amount,"\n",water.name,water.amount,"\n",zanahorias.name,zanahorias.amount)
-
     print(papas)
     print(zanahorias)
