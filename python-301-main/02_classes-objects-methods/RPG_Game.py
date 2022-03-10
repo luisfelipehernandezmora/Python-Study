2e96526d74995bb25828fad0e5cc6375dc0f08d5
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2022-03-09 21:05:27 -0600

    assembly of the Roll the dice mechanism

--- python-301-main/02_classes-objects-methods/RPG_Game.py
+++ python-301-main/02_classes-objects-methods/RPG_Game.py
@@ -20,17 +20,30 @@ class Hero():
         self.strength=strength
         self.bag=bag
 
-    def __str__():
+    def __str__(self):
         return(f"Hero(name={self.name}, level={self.level}, strength={self.strength}, bag={self.bag})")
 
     def attack(self,other):
         return(f"Something")
 
     def roll_dice(self,other):
-        rand_num=random.randint(1,6)
-        
+        rand_num1=round(random.randint(1,10)*self.level/10,0)
+        rand_num2=round(random.randint(1,10)*other.level/10,0)
+        if rand_num1>rand_num2:
+            other.strength-=self.level/10
+            self.level+=1
+            print(f"Well done {self.name} you won over {other.name}!")
+        elif rand_num2>rand_num1:   
+            self.strength-=other.level/10
+            other.level+=1
+            print(f"Well done {other.name} you won over {self.name}!")
+        elif rand_num2==rand_num1:
+            print(f"Wow good luck! you both better run!")
 
-
-
-
-class Opponent()
+luke=Hero("Luke",12,10,["sword","spell"])
+vader=Hero("Vader",15,9,["sword","laser"])
+i=0
+while luke.level<25 and vader.level<25:
+    atack=luke.roll_dice(vader)
+    print(atack, luke, vader,i)
+    i+=1
