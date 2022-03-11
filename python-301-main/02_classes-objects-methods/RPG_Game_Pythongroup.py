3a5a053ccafd8d87f3ebb9270cb6b3c5f5e70ed4
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2022-03-10 22:39:24 -0600

    RPG game mostly done, some more excitement can be included

--- python-301-main/02_classes-objects-methods/RPG_Game_Pythongroup.py
+++ python-301-main/02_classes-objects-methods/RPG_Game_Pythongroup.py
@@ -80,6 +80,9 @@ class Hero():
         elif rand_num2==rand_num1:
             print(f"Wow good luck! you both better run!")
 
+    def check(self):
+        print(f"Your actual status is {self}")
+
 class Opponent():
     """Class to define the Opponent
 
@@ -105,6 +108,81 @@ class Opponent():
 
 luke=Hero("Luke",12,10,["sword","spell"],True)
 vader=Opponent("Vader",15,9,["sword","laser"],True)
+figther1=Opponent("Jack",10,9,["sword"],True)
+fighter2=Opponent("Red sea",9,9,["sword"],True)
+fighter3=Opponent("Star Fish",11,9,["sword"],True)
+fighter4=Opponent("Death star",12,9,["sword"],True)
+oponentes=[vader,figther1, fighter2, fighter3, fighter4]
 
-#luke.run_away(vader)
-luke.attack(vader)
+# print(len(oponentes))
+while len(oponentes)>0:
+    ask=input(f"{luke.name} You want to walk front, left or rigth? or you want to check your health? (check) ")
+    if ask=="front":
+        destiny=random.randint(1,2)
+
+        if destiny ==1:
+            ask2=input(f"Oh, you found an Opponent, you want to attack or run away? ")
+            current_enemy=random.choice(oponentes)
+            if ask2=="attack":
+                luke.attack(current_enemy)
+                if current_enemy.active==False:
+                    oponentes.remove(current_enemy)
+                    print(f"{len(oponentes)} left in the field")
+                continue
+            elif ask2=="run away":
+                luke.run_away(current_enemy)
+                if current_enemy.active==False:
+                    oponentes.remove(current_enemy)
+                    print(f"{len(oponentes)} left in the field")
+                continue
+        if destiny ==2:
+            print(f"Good news, the path is free for now, keep exploring")
+            continue
+
+    elif ask=="left":
+        destiny=random.randint(1,2)
+
+        if destiny ==1:
+            ask2=input(f"Oh, you found an Opponent, you want to attack or run away? ")
+            current_enemy=random.choice(oponentes)
+            if ask2=="attack":
+                luke.attack(current_enemy)
+                if current_enemy.active==False:
+                    oponentes.remove(current_enemy)
+                    print(f"{len(oponentes)} left in the field")
+                continue
+            elif ask2=="run away":
+                luke.run_away(current_enemy)
+                if current_enemy.active==False:
+                    oponentes.remove(current_enemy)
+                    print(f"{len(oponentes)} left in the field")
+                continue
+        if destiny ==2:
+            print(f"Good news, the path is free for now, keep exploring")
+            continue
+
+    elif ask=="rigth":
+        destiny=random.randint(1,2)
+
+        if destiny ==1:
+            ask2=input(f"Oh, you found an Opponent, you want to attack or run away? ")
+            current_enemy=random.choice(oponentes)
+            if ask2=="attack":
+                luke.attack(current_enemy)
+                if current_enemy.active==False:
+                    oponentes.remove(current_enemy)
+                    print(f"{len(oponentes)} left in the field")
+                continue
+            elif ask2=="run away":
+                luke.run_away(current_enemy)
+                if current_enemy.active==False:
+                    oponentes.remove(current_enemy)
+                    print(f"{len(oponentes)} left in the field")
+                continue
+        if destiny ==2:
+            print(f"Good news, the path is free for now, keep exploring")
+            continue
+    elif ask=="check":
+        luke.check()    
+print(f"Wow can't believe it, you really made it trough, congrats! You won")
+
