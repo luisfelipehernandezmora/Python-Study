130d99e8d6f83a8f4f90d7fd428a980bdd2bafb2
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2022-03-19 15:38:10 -0600

    Include used and missed ingredients

--- python-301-main/03_inheritance/Get_recipes.py
+++ python-301-main/03_inheritance/Get_recipes.py
@@ -6,27 +6,42 @@ import webbrowser
 key=os.environ["keyspoon"]
 num_rec=5
 folder="/home/luisfelipe/Coding Nomads/python-301-main/03_inheritance/sample.json"
-with open(folder,"r") as file:
+with open(folder,"r") as file: #Reads the json file already done (to minimize multiple unnecesary callings)
     recipe=json.load(file)
 
 cooking_book={}
 ids=[]
-for i in range (5):
+for i in range (5): #Maps the recipes availables and Id number
     title=recipe[i]["title"]
     recipe_id=recipe[i]["id"]
+    missed_ingredients=recipe[i]["missedIngredients"]
+    used_ingredients=recipe[i]["usedIngredients"]
     cooking_book[recipe_id]=title
     ids.append(recipe_id)
     print(f"This recipe is called {title} and have the id {recipe_id}")
+    for i in range(len(missed_ingredients)):
+        missed=missed_ingredients[i]["name"]
+        #print(f"missed {missed}")
+    for i in range(len(used_ingredients)):
+        used=used_ingredients[i]["name"]
+        #print(f"used {used}")
 
-ask=input(f"You want to look for any of these recipes procedures? ")
+ask=input(f"You want to look for any of these recipes procedures? ") #This section looks for the recipe
 if ask == "Yes":
-    ask_id=int(input(f"Oh right! tell me the id of that recipe "))
+    ask_id=int(input(f"Oh right! tell me the id of that recipe \n")) #The user choose which recipe to look upon
     if ask_id in ids:
         #https://api.spoonacular.com/recipes/782619/information?apiKey=f1727df4f2004de98168f2021f8f3c87&includeNutrition=false
         url="https://api.spoonacular.com/recipes/"+str(ask_id)+"/information?apiKey="+key+"&includeNutrition=false"
         pasos=requests.get(url).json()["instructions"]
-        print(pasos)
-        print("Voila Bonne Apettite you have now the full steps on how to proceed, salud!")
+        site=requests.get(url).json()["sourceUrl"]
+        print(pasos,"\n")
+        print("Voila Bonne Apettite you have now the full steps on how to proceed, salud! \n also let me open the webpage for you ... \n")
+        webbrowser.open(site, new=0, autoraise=True)
+
+
+        
+
+
 else:
     print(f"ok, you have the recipes, just come back whenever you want ")
     
--- python-301-main/03_inheritance/Ingredients.py
+++ python-301-main/03_inheritance/Ingredients.py
@@ -85,13 +85,14 @@ lista=["bananas", "peanuts", "oats", "milk", "yogurt", "strawberries", "cacao po
 lista2=["papaya", "milk", "oats", "granola", "cereal", "strawberries", "coffee"]
 lista3=["eggs", "bread", "tomato", "mushrooms", "coffee", "strawberries", "cinamon"]
 
-recipes=look_recipe(lista2)
-pprint(recipes)
-json_object=json.dumps(recipes,indent=4)
+# recipes=look_recipe(lista)
+# pprint(recipes)
+# json_object=json.dumps(recipes,indent=4)
 
-folder="/home/luisfelipe/Coding Nomads/python-301-main/03_inheritance/sample.json"
-with open(folder,"w") as outfile:
-    outfile.write(json_object)
+# folder="/home/luisfelipe/Coding Nomads/python-301-main/03_inheritance/sample.json"
+# with open(folder,"w") as outfile:
+#     outfile.write(json_object)
 
 banana=Ingredient("bananas",2)
 print(banana.get_info())
+
