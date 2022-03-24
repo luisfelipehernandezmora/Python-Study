75d1408f5609696f2ce74cb87428170855f2cfa3
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2022-03-23 22:47:50 -0600

    Parsing from Codign Nomads recipes

--- python-301-main/04_web-scraping/Sandbox.py
+++ python-301-main/04_web-scraping/Sandbox.py
@@ -1,5 +1,14 @@
 import requests
+from bs4 import BeautifulSoup
 
 url="https://codingnomads.github.io/recipes/"
-page=requests.get(url)
-print(page.text)
+url2="https://codingnomads.github.io/recipes/recipes/68-kimchi-fried-rice-wi.html"
+page=requests.get(url2).text
+#print(page,type(page))
+
+soup=BeautifulSoup(page,features="html.parser")
+print(soup.prettify)
+
+# links=soup.find_all("a")
+# for link in links:
+#     print(link["href"])
