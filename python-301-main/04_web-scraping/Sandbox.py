1d3f2c2f0e2799e334d9470b07d254cbfaea79e8
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2022-03-24 21:35:54 -0600

    Get title from recipe

--- python-301-main/04_web-scraping/Sandbox.py
+++ python-301-main/04_web-scraping/Sandbox.py
@@ -1,14 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
+from pprint import pprint
 
-url="https://codingnomads.github.io/recipes/"
+# # from the index get the links
+# url="https://codingnomads.github.io/recipes/"
+# page=requests.get(url).text
+# # print(page)
+# soup=BeautifulSoup(page,features="html.parser")
+# links=soup.find_all("a")
+# for link in links:  #Get HTML's here
+#     print(link["href"])
+
+# Get the Title, author and text describing the recipe
 url2="https://codingnomads.github.io/recipes/recipes/68-kimchi-fried-rice-wi.html"
 page=requests.get(url2).text
-#print(page,type(page))
-
 soup=BeautifulSoup(page,features="html.parser")
-print(soup.prettify)
-
-# links=soup.find_all("a")
-# for link in links:
-#     print(link["href"])
+#print(soup.prettify)
+#print(soup)
+title=soup.find("h1",class_="title").text
+print(title)
