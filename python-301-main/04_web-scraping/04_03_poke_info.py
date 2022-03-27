c9ed2ea042f9c042f14ef679da0061eee963386c
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2022-03-26 21:22:45 -0600

    Getting the information of 1 pokemon

--- python-301-main/04_web-scraping/04_03_poke_info.py
+++ python-301-main/04_web-scraping/04_03_poke_info.py
@@ -10,5 +10,18 @@
 # You can improve on your project even more by writing the data to a small
 # `.html` page which allows you to also display the sprites of each Pokémon.
 # Check out the guides they provide: https://pokeapi-how.appspot.com/page5
-
+from pprint import pprint
+import requests
+import json
 BASE_URL = "https://pokeapi.co/api/v2/"
+url="https://pokeapi.co/api/v2/pokemon/1/"
+folder="/home/luisfelipe/Coding Nomads/python-301-main/04_web-scraping/temp_pokemon.json"
+# pokemon=requests.get(url).json()
+# with open(folder,"w") as file:
+#     json.dump(pokemon,file)
+with open(folder,"r") as file:
+    data=json.load(file)
+name=data["forms"]
+
+# pprint(data)
+# print(type(data))
