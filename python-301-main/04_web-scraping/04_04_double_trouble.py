450ab801e3243a1ec44c70f1f36725b242a20177
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2022-04-16 14:46:37 -0600

    Coming back to study

--- python-301-main/04_web-scraping/04_04_double_trouble.py
+++ python-301-main/04_web-scraping/04_04_double_trouble.py
@@ -4,3 +4,4 @@
 # data that you receive from both of them.
 # E.g.: You could use the Ghibli API to find all ghosts from their films, and
 #       create an opposing team of Ghost Pokémon from the Poke API.
+import requests
