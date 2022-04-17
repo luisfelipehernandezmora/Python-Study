b552f8f102b090599c719ab829d6d45efbb76265
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2022-04-17 14:34:29 -0600

    Request of artist done, include search of country

--- python-301-main/04_web-scraping/04_04_double_trouble.py
+++ python-301-main/04_web-scraping/04_04_double_trouble.py
@@ -5,3 +5,25 @@
 # E.g.: You could use the Ghibli API to find all ghosts from their films, and
 #       create an opposing team of Ghost Pokémon from the Poke API.
 import requests
+import json 
+from pprint import pprint
+
+#1) Select an artist and research about  
+ask=input(f"From which artist you will like to know about? ")
+
+url= f"https://theaudiodb.com/api/v1/json/2/search.php?s={ask}"
+artist=requests.get(url).json()
+#pprint(artist)
+
+#2) For the sake of fewer requests, create a Json with the content of the request
+folder="/Users/flormariamorafallas/Desktop/CodingNomads/Python-Study/python-301-main/04_web-scraping/artist.json"
+# with open(folder,"w") as file:
+#     json.dump(artist,file)
+
+with open(folder,"r") as file:
+    data=json.load(file)
+#pprint(data)
+
+#3) Make the Calls
+name=data["artists"][0]["strArtist"]
+print(name)
