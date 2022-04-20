9a3b6d24e3980d9574e06523fc152f3e5868fa7d
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2022-04-20 15:26:02 -0600

    update

--- python-301-main/04_web-scraping/04_05_wiki_scrape.py
+++ python-301-main/04_web-scraping/04_05_wiki_scrape.py
@@ -8,13 +8,43 @@
 url = "https://en.wikipedia.org/wiki/Web_scraping"
 import requests
 from bs4 import BeautifulSoup
+from random import randint
 
-folder="/Users/flormariamorafallas/Desktop/CodingNomads/Python-Study/python-301-main/04_web-scraping/wikipedia_articles"
 
+def wikipedia_article(link):
+    """Function to determine if a hyperlink goes to a wikipedia article
+
+    Args:
+        1st arg: url of the link
+                
+    Returns:
+    True if the link goes to a wikipedia article
+    """ 
+    flag=False
+    if link[:6]=="/wiki/":
+        flag=True
+    return flag
+
+folder="/Users/flormariamorafallas/Desktop/CodingNomads/Python-Study/python-301-main/04_web-scraping/wikipedia_articles/random_wiki_art.rtf"
 page=requests.get(url).text
 soup=BeautifulSoup(page, features="html.parser")
-
 links=soup.find_all("a")
+wiki_arts=[]
+
 for link in links:
-    print(link)
-    #endpoint=link["href"]
+    #print(link,'\n')
+    endpoint=str(link.get('href'))
+    if wikipedia_article(endpoint):
+        link2="https://en.wikipedia.org"+endpoint
+        wiki_arts.append(link2)
+        #print(endpoint)
+
+num=randint(0,len(wiki_arts)-1)
+#print(len(wiki_arts), num)
+
+for i in wiki_arts:
+    print(i)
+
+mine=requests.get(wiki_arts[num]).text
+with open (folder,"w") as file:
+    file.write(mine)
