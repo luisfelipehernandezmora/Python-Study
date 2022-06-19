824e751bcc36a87002decdee47a2051504d02730
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2022-06-19 14:13:13 +0530

    new tool file

---
+++ python-301-main/04_web-scraping/web_tools.py
@@ -0,0 +1,9 @@
+import requests
+import json
+
+base_url="https://ghibliapi.herokuapp.com"
+movies=base_url+"/films"
+people=base_url+"/people"
+locations=base_url+"/locations"
+species=base_url+"/films"
+vehicles=base_url+"/films"
