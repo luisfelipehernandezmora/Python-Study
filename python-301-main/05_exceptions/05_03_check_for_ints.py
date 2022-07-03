ee2d818b24eed542fa0da7791acbfce24854cbf5
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2022-07-03 22:25:42 +0530

    excercises 3 and 4 done

--- python-301-main/05_exceptions/05_03_check_for_ints.py
+++ python-301-main/05_exceptions/05_03_check_for_ints.py
@@ -2,3 +2,11 @@
 # validity of the input type, and displays a message depending on whether
 # the input was an integer or not.
 # The script should keep prompting the user until they enter an integer.
+
+while True:
+    try:
+        ask=int(input(f"Give me a int: "))
+        print("You see was no so hard! ")
+        break
+    except ValueError:
+        print("it got it be an int ")
--- python-301-main/05_exceptions/05_04_else_database.py
+++ python-301-main/05_exceptions/05_04_else_database.py
@@ -2,3 +2,17 @@
 # For example, you can revisit the course module about database interactions
 # and include a try/except/else statement based on what to do whether or not
 # the database connection can be established.
+
+import webbrowser
+while True:
+    try:
+        web=input("Insert a wikipedia website you want to open ")
+        if "/wiki/" not in web:
+            raise Exception("Oh is not a wikipedia link")
+    except Exception:
+        print("it should be a wikipedia link, try again ")
+        continue
+    else:
+        print("good job! let's go to the page ")
+        webbrowser.open(web,new=0,autoraise=True)
+        break
