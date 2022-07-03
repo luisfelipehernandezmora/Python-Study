4404652622ee5f6cfd92300739d0138732c7ec1e
luisfelipehernandezmora <luisfelipehernandezmora@gmail.com>
2022-07-03 16:35:07 +0530

    Exception exercises 1 and 2

--- python-301-main/05_exceptions/05_01_first_exception.py
+++ python-301-main/05_exceptions/05_01_first_exception.py
@@ -6,3 +6,22 @@
 # print(list_[1])
 #
 # This raises and exception that needs to be handled.
+while True:
+    try:
+        ask=int(input(f"From 0 to 10, how big is your hunger? "))
+        if 0<ask<5:
+            print(f"You can eat something small and healthy until next meal")
+            break
+        elif 11>ask>=5:
+            print(f"You can go for a full meal")
+            break
+        else:
+            raise Exception 
+    except ValueError:
+        print("please put numbers")
+        continue
+    except Exception:
+        print("numbers between the range please")
+        continue
+
+
--- python-301-main/05_exceptions/05_02_calculator.py
+++ python-301-main/05_exceptions/05_02_calculator.py
@@ -5,3 +5,17 @@
 # - if the user enters a zero as the divisor
 #
 # Test it and make sure it does not crash when you enter incorrect values.
+
+while True:
+    try:
+        num1=int(input(f"Please provide the number to be divided: "))
+        num2=int(input(f"Please provide the number divide: "))
+        res=num1/num2
+        print(f"Your division is {res} yay!")
+        break
+    except ValueError:
+        print(f"Upi have to enter numbers not something else")    
+        continue
+    except ZeroDivisionError:
+        print(f"Oh nonono dont divide by zero, try again :D")
+        continue
